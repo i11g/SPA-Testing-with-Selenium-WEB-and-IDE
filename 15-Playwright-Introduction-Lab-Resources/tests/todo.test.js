b4c6e5801a47e62283e2675727c6b50d5a69d586
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-12 15:23:57 +0300

    files changed

--- 15-Playwright-Introduction-Lab-Resources/tests/todo.test.js
+++ 15-Playwright-Introduction-Lab-Resources/tests/todo.test.js
@@ -1,4 +1,4 @@
-const {test, expect} = require('@playwright/test') 
+const {test, expect} = require ('@playwright/test') 
 
 //verify that user can add task 
 
@@ -16,7 +16,9 @@ test("user can add task", async ({page})=>{
     expect(taskText).toContain('Test Task') 
 }) 
 
-test ("user can dlete task", async ({page})=>{
+//verify that user can delete task
+
+test ("user can delete task", async ({page})=>{
     
     //arrange
     await page.goto('http://localhost:8080')
@@ -27,9 +29,43 @@ test ("user can dlete task", async ({page})=>{
     await page.click('.task .delete-task')
 
     //assert 
+    //take all <li> elements
     const tasks=await page.$$eval('.tasks', tasks=>tasks.map (
         task=> task.textContent
     )) 
-    expect(tasks).not.toContain("Test Task")
-
+    expect(tasks).not.toContain("Test Task") 
 })
+
+    //verify that a user can mark a task as complete 
+
+    test ("user can mark a task as complete", async ({page})=>{
+        
+        //arrange
+        await page.goto('http://localhost:8080')
+        await page.fill('#task-input', 'Test Task')
+        await page.click('#add-task') 
+
+        //act
+        await page.click('.task .task-complete' ) 
+
+        //assert
+        const completedTask= await page.$('.task.completed')
+        expect(completedTask).not.toBeNull()  
+    }) 
+
+    //verify that a user can filter tasks 
+
+    test("user can filter tasks", async ({page})=>{
+        //Arrange
+        await page.goto('http://localhost:8080')
+        await page.fill('#task-input', 'Test Task')
+        await page.click('#add-task')
+        await page.click('.task .task-complete')
+
+        //act
+        await page.selectOption('#filter', 'Completed') 
+
+        //assert
+        let incompletedTask=await page.$('.task:not(.completed)')
+        expect(incompletedTask).toBeNull()
+    })
