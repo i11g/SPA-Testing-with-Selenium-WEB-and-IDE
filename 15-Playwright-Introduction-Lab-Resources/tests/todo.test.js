4f2a0a396f4bec77bf4c40bc6f2f3eb59162030d
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-12 06:53:15 +0300

    files chnaged

--- 15-Playwright-Introduction-Lab-Resources/tests/todo.test.js
+++ 15-Playwright-Introduction-Lab-Resources/tests/todo.test.js
@@ -13,6 +13,23 @@ test("user can add task", async ({page})=>{
 
     //assert 
     const taskText= await page.textContent('.task')
-    expect(taskText).toContain('Test Task')
+    expect(taskText).toContain('Test Task') 
+}) 
+
+test ("user can dlete task", async ({page})=>{
+    
+    //arrange
+    await page.goto('http://localhost:8080')
+    await page.fill('#task-input', 'Test Task')
+    await page.click('#add-task')
+
+    //act 
+    await page.click('.task .delete-task')
+
+    //assert 
+    const tasks=await page.$$eval('.tasks', tasks=>tasks.map (
+        task=> task.textContent
+    )) 
+    expect(tasks).not.toContain("Test Task")
 
 })
